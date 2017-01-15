686fb5b13d5216626edbf0741bf5392a78252337
anmatika <4ntti.matikainen@gmail.com>
2017-01-15 11:53:06 +0200

    chai as prom

--- spec/dns.spec.js
+++ spec/dns.spec.js
@@ -1,12 +1,24 @@
 const dns = require('../app/dns');
-const expect = require('chai').expect;
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+chai.use(require('chai-string'));
+const expect = chai.expect;
+chai.should();
 
 describe('dns', () => {
     const expectedIp = '104.20.22.46';
     it('should resolve correct ip', (done) => {
-        const lookup = dns.lookup('nodejs.org').then((lookup)=> {
-            expect(lookup).to.equal(expectedIp);
-        }).done(() => done());
+        const lookup = dns.lookup('nodejs.org')
+            .then((lookup) => {
+                expect(lookup).to.equal(expectedIp);
+            })
+            .done(() => done());
     });
 
+    it('should resolve correct ip 2', () => {
+        return dns.lookup('nodejs.org')
+            .should.eventually.equal(expectedIp)
+
+    });
 });
