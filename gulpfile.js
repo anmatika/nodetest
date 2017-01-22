462adec077a4789e83ba728b2024ceab7cf58fe5
anmatika <4ntti.matikainen@gmail.com>
2017-01-22 17:34:12 +0200

    use arrow syntax

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,20 @@
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
 const gutil = require('gulp-util');
-const rename = require("gulp-rename");
-const gulpif = require('gulp-if');
-const tap = require('gulp-tap');
-const ignore = require('gulp-ignore')
-const util = require('util');
+const rename = require('gulp-rename');
+const ignore = require('gulp-ignore');
 const clean = require('gulp-clean');
 
-gulp.task('watch-mocha', function() {
+gulp.task('watch-mocha', () => {
     // run in oneshot
     gulp.start('mocha');
     // start watching
     gulp.watch(['app/**', 'spec/**'], ['mocha']);
 });
 
-gulp.task('mocha', function() {
+gulp.task('mocha', () => {
     return gulp.src(['spec/*.js'], {
-            read: false
+        read: false
         })
         .pipe(mocha({
             reporter: 'list'
@@ -33,13 +30,13 @@ gulp.task('rename', () => {
     return gulp.src('./app/*.js')
         .pipe(ignore.include(file =>
             scriptFiles.some(scriptFile =>
-                             file.path.split('/').pop() === scriptFile)))
+                file.path.split('/')
+                .pop() === scriptFile)))
         .pipe(clean())
         .pipe(rename({
-            suffix: "-someSuffix",
+            suffix: '-someSuffix'
         }))
         .pipe(gulp.dest(file => file.base)); /* leave it in the same location */
 });
-
 
 gulp.task('default', ['watch-mocha']);
